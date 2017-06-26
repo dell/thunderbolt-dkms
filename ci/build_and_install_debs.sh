99bdb864f6e0e53ac8ad5ae853ba77dcecd57a74
Mario Limonciello <mario.limonciello@dell.com>
2017-06-26 17:22:00 -0500

    run CI on generation of the package

---
+++ ci/build_and_install_debs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+#build deb packages
+VERSION=`git describe | sed 's/-/+r/;s/-/+/'`
+[ -z $VERSION ] && VERSION=`dpkg-parsechangelog -S version`
+
+rm -rf build/
+CP=`ls`
+mkdir -p build
+cp -R $CP build
+pushd build
+rm debian/changelog -f
+EDITOR=/bin/true dch --create --package thunderbolt-dkms -v $VERSION "CI Build"
+dpkg-buildpackage
+
+#test the packages install
+dpkg -i ../*.deb
+
+dkms mkrpm -m thunderbolt -v $VERSION
